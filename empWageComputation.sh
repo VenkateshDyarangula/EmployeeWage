46a2ecd59b09c5fc7dd37d0b15f1441eed1ab1a7
venkatesh <venkateshdyarangula@gmail.com>
2021-02-04 18:38:30 +0530

    Refactor - attendance finctionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
-echo "Hello EmpDeatils"
+echo "Hello EmpDeatils Program"
+
+isPresent=1;
+checkRandom=$((RANDOM%2))
+if [ $isPresent -eq $checkRandom ]
+then
+echo "Emp is Present"
+else
+echo "Emp is Absent"
+fi
