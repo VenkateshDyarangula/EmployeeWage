45c95e0936257afe1d7e575b07b4db88a03d5952
venkatesh179 <venkateshdyarangula@gmail.com>
2021-02-07 19:40:26 +0530

    a function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash 
 #UC1
 echo "Hello Welcome to EmpDeatils Programe"
 isPresent=1;
-wagePerHr=20;
-fullDay=8;
 dailyPayment=0;
-fulltime=5;
-parttime=3;
 monthDays=20;
 workHrs=100;
+count=1;
+Daycount=1;
 checkRandom=$((RANDOM%2))
 partFullCheck=$((RANDOM%2))  #UC-4 (0-FullTime 1-PartTime)
+#UC-7
+getWorkHrs () {
+for (( i=1; i<=$monthDays; i++ ))
+do
+DayHrs=$(($workHrs/$monthDays))
+DayHrs=$(($DayHrs*$Daycount))
+echo "$count day Work Hrs" $DayHrs
+((count++))
+((Daycount++))
+done
+}
+
 if [ $isPresent -eq $checkRandom ]   
 then
      echo "Emp is Present"
@@ -18,24 +28,12 @@ then
       case $partFullCheck in    #(0-FullTime and 1-PartTime)
             #UC-2 and UC-5
     
-         0)  for (( i=1; i<=$workHrs; i++ ))#UC-6
-             do
-                   Day=$(($workHrs/$monthDays))
-                   dailyPayment=$(($wagePerHr*$i*$Day))
-                   echo "Daily Payment of Full Time is:" $dailyPayment
-                  
-             done
-                ;;
-
-
-             #UC3 and UC-5
-         1)  for (( i=1; i<=$workHrs; i++ ))#UC-6
-             do
-                   Day=$(($workHrs/$monthDays-2))
-                   dailyPayment=$(($wagePerHr*$i*$Day))
-                   echo "Daily Payment of Part Time is:" $dailyPayment
-                   
-             done
+         0)  #UC7
+                   getWorkHrs
+                   ;;
+         1) #UC7
+                   getWorkHrs
+               
                ;;
       esac
     
